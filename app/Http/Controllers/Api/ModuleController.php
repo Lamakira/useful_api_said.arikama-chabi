42b913bd452b37b141e74ae2b1e45754b96afaec
Lamakira <said.arikama-chabi@epitech.eu>
2025-10-16 12:57:41 +0100

    feat: add get all modules endpoint

---
+++ app/Http/Controllers/Api/ModuleController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Module;
+use App\Models\UserModule;
+use Illuminate\Http\Request;
+
+class ModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $allModules = Module::all(['id', 'name', 'description']);
+        return response()->json($allModules);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function activate(Request $request, Module $module)
+    {
+        $moduleId = $module->id;
+        $realModule = Module::findOrfail($moduleId);
+        if(!$realModule)
+        {
+            return response()->json([], 400);
+        }
+
+        $userId = $request->user()->id;
+
+        UserModule::create([
+            "user_id" => $userId,
+            "module_id" => $moduleId,
+            "active" => true
+        ]);
+
+        return response()->json([
+            'message' => "Module activated"
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ModuleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,9 +9,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//Login and register routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
 Route::middleware('auth:sanctum')->group(function () {
+    //to logout
    Route::get('/logout', [AuthController::class, 'logout']);
+
+   //get all modules
+   Route::get('/modules', [ModuleController::class, 'index']);
+   Route::post('/modules/{module}/activate', [ModuleController::class, 'activate']);
 });
